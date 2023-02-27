d311298aeee1e0ed46c26e2230312d5cb97b69e3
WooJeong <cincu4221@naver.com>
2023-02-27 13:57:45 +0900

    ch05-06 clear

---
+++ tests/example.test.js
@@ -0,0 +1,8 @@
+import { asyncFn } from  './example'
+
+describe('비동기 테스트', () => {
+  test('async/await', async () => {
+    const res = await asyncFn()
+    expect(res).toBe('Done!')
+  }, 7000)
+})
